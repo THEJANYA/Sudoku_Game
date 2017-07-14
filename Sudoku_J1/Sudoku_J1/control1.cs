00482a28e6ab590a281b1546031c6818471756a6
Janya Buangam <janya.buangam@gmail.com>
2017-07-14 18:02:10 +0700

    not yet complete

---
+++ Sudoku_J1/Sudoku_J1/control1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sudoku_J1
+{
+
+    class control1
+    {
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          #region fied
+        private string _txt1;
+        private string _txt2;
+        private string _txt3;
+        private string _txt4;
+        private string _txt5;
+        private string _txt6;
+        private string _txt7;
+        private string _txt8;
+        private string _txt9;
+
+        #endregion
+
+        #region Property
+
+        public string txt1
+        {
+            get
+            {
+                return this._txt1;
+            }
+            set
+            {
+                this._txt1 = value;
+            }
+        }
+        public string txt2 { 
+            get
+            {
+                return this._txt2;
+            }
+            set
+            {
+                this._txt2 = value;
+            }
+        }
+        public string txt3
+        {
+            get
+            {
+                return this._txt3;
+            }
+            set
+            {
+                this._txt3 = value;
+            }
+        }
+
+        public string txt4
+        {
+            get
+            {
+                return this._txt4;
+            }
+            set
+            {
+                this._txt4 = value;
+            }
+        }
+
+        public string txt5
+        {
+            get
+            {
+                return this._txt5;
+            }
+            set
+            {
+                this._txt5 = value;
+            }
+        }
+
+        public string txt6
+        {
+            get
+            {
+                return this._txt6;
+            }
+            set
+            {
+                this._txt6 = value;
+            }
+        }
+        public string txt7
+        {
+            get
+            {
+                return this._txt7;
+            }
+            set
+            {
+                this._txt7 = value;
+            }
+        }
+
+        public string txt8
+        {
+            get
+            {
+                return this._txt8;
+            }
+            set
+            {
+                this._txt8 = value;
+            }
+        }
+
+        public string txt9
+        {
+            get
+            {
+                return this._txt9;
+            }
+            set
+            {
+                this._txt9 = value;
+            }
+        }
+
+
+
+        #endregion
+
+        //public static void ChkBox1 ( EnvironmentVariableTarget as control1  )
+        //{
+        //   // return ChkBox1();
+        //}
+
+    }
+}
